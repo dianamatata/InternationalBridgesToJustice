e8f9044aafadcdf06b37bfb3ca5347b2807b8cd6
dianamatata <diana.avalos.m@gmail.com>
2025-04-07 15:11:01 +0200

    improved

--- count_chunks.py
+++ count_chunks.py
@@ -1,22 +1,15 @@
 import json
 
+path1 = "data/processed/defensewiki.ibj.org/chunks.jsonl"
+path2 = "data/processed/constituteproject.org/constitution_chunks.jsonl"
+chunks = []
+for path in [path1, path2]:
+    with open(f"{path}", "r", encoding="utf-8") as jsonl_file:
+        lines = jsonl_file.readlines()  # Read all lines once
+        print(f"Number of lines in the jsonl file {path}: {len(lines)}")
+        for line in lines:
+            chunks.append(json.loads(line))
+    print(f"Total number of chunks: {len(chunks)}")  # Should be 50380
 
-path = "data/processed/defensewiki.ibj.org"
-
-with open(f"{path}/chunks.jsonl", "r", encoding="utf-8") as jsonl_file:
-    line_count = sum(1 for line in jsonl_file)
-    print(f"Number of lines in the jsonl file: {line_count}")
-
-path2 = "data/processed/constituteproject.org"
-with open(f"{path2}/constitution_chunks.jsonl", "r", encoding="utf-8") as jsonl_file:
-    line_count2 = sum(1 for line in jsonl_file)
-    print(f"Number of lines in the jsonl file: {line_count2}")
-
-print(f"Total number of chunks: {line_count + line_count2}") # 50380 chunks in total of 500 words
-
-
-# print first line of json file
-with open(f"{path}/chunks.jsonl", "r", encoding="utf-8") as jsonl_file:
-    first_line = jsonl_file.readline()
-    print(json.loads(first_line))
+chunks[0]
 
