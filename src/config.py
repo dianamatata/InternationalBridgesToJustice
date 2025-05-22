3c1fe068e2b1b61975859e2b8decdfcfa9e1adb9
dianamatata <diana.avalos.m@gmail.com>
2025-05-22 13:43:28 +0200

    file paths in majuscules

--- src/config.py
+++ src/config.py
@@ -1,39 +1,43 @@
-# embeddings, collection, chromadb
-path_chromadb = "data/chroma_db"
-collection_name = "legal_collection"
-path_jsonl_file_raw_embeddings = "data/chroma_db/raw_embeddings.jsonl"
+class Paths:
 
-# DefenseWiki
-base_url_defense_wiki = "https://defensewiki.ibj.org"
-path_folder_defense_wiki = "data/processed/defensewiki.ibj.org"
-path_jsonl_file_defensewiki_chunks = "data/processed/defensewiki.ibj.org/chunks_1.jsonl"
-path_jsonl_file_defensewiki = "data/interim/defensewiki_all.jsonl"
+    # embeddings, collection, chromadb
+    PATH_CHROMADB = "data/chroma_db"
+    COLLECTION_NAME = "legal_collection"
+    PATH_JSONL_FILE_RAW_EMBEDDINGS = "data/chroma_db/raw_embeddings.jsonl"
+
+    # DefenseWiki
+    BASE_URL_DEFENSE_WIKI = "https://defensewiki.ibj.org"
+    PATH_FOLDER_DEFENSE_WIKI = "data/processed/defensewiki.ibj.org"
+    PATH_JSONL_FILE_DEFENSEWIKI_CHUNKS = "data/processed/defensewiki.ibj.org/chunks_1.jsonl"
+    PATH_JSONL_FILE_DEFENSEWIKI = "data/interim/defensewiki_all.jsonl"
 
 
-# ConstitutionProject
-start_url_constitutions = "https://www.constituteproject.org/constitutions?lang=en&status=in_force&status=is_draft"
-base_url_constitutions = "https://www.constituteproject.org"
-path_folder_constitutions_with_md_files = "data/raw/constituteproject.org"
-path_folder_constitutions_with_jsonl_files = "data/processed/constituteproject.org"
-path_jsonl_file_with_constitutions_all_countries = f"{path_folder_constitutions_with_jsonl_files}/constituteproject.jsonl"
-path_jsonl_file_constitution_chunks = "data/processed/constituteproject.org/constitution_chunks.jsonl"
+    # ConstitutionProject
+    START_URL_CONSTITUTIONS = "https://www.constituteproject.org/constitutions?lang=en&status=in_force&status=is_draft"
+    BASE_URL_CONSTITUTIONS = "https://www.constituteproject.org"
+    PATH_FOLDER_CONSTITUTIONS_WITH_MD_FILES = "data/raw/constituteproject.org"
+    PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES = "data/processed/constituteproject.org"
+    PATH_JSONL_FILE_WITH_CONSTITUTIONS_ALL_COUNTRIES = f"{PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES}/constituteproject.jsonl"
+    PATH_JSONL_FILE_CONSTITUTION_CHUNKS = "data/processed/constituteproject.org/constitution_chunks.jsonl"
 
-# Keypoints completeness
-path_md_file_completeness_keypoints = "data/raw/IBJ_docs/Completeness_checklist.md"
-path_json_file_completeness_keypoints = "data/completeness/keypoints.json"
-path_folder_completeness = "data/completeness"
+    # Keypoints completeness
+    PATH_MD_FILE_COMPLETENESS_KEYPOINTS = "data/raw/IBJ_docs/Completeness_checklist.md"
+    PATH_JSON_FILE_COMPLETENESS_KEYPOINTS = "data/completeness/keypoints.json"
+    PATH_FOLDER_COMPLETENESS = "data/completeness"
 
-# Claim extraction
-path_folder_claim_extraction = "data/extracted_claims"
-path_jsonl_file_extracted_claims = f"{path_folder_claim_extraction}/claims_chunks_1.jsonl"
+    # Claim extraction
+    PATH_FOLDER_CLAIM_EXTRACTION = "data/extracted_claims"
+    PATH_JSONL_FILE_EXTRACTED_CLAIMS = f"{PATH_FOLDER_CLAIM_EXTRACTION}/claims_chunks_1.jsonl"
 
-# Prompts
-path_prompts = "data/prompts"
-path_file_prompt_completeness = f"{path_prompts}/prompt_completeness.md"
-path_file_prompt_claim_extraction = f"{path_prompts}/prompt_claim_extraction.md"
-path_file_prompt_claim_verification = f"{path_prompts}/prompt_claim_verification.md"
-path_file_system_prompt_completeness = f"{path_prompts}/system_prompt_completeness.md"
+    # Prompts
+    PATH_PROMPTS = "data/prompts"
+    PATH_FILE_PROMPT_COMPLETENESS = f"{PATH_PROMPTS}/prompt_completeness.md"
+    PATH_FILE_PROMPT_CLAIM_EXTRACTION = f"{PATH_PROMPTS}/prompt_claim_extraction.md"
+    PATH_FILE_PROMPT_CLAIM_VERIFICATION = f"{PATH_PROMPTS}/prompt_claim_verification.md"
+    PATH_FILE_SYSTEM_PROMPT_COMPLETENESS = f"{PATH_PROMPTS}/system_prompt_completeness.md"
+
+    FILE_COUNTRY_NAMES = "data/interim/country_names_1.txt"
+
 
 # Others
 MAX_CHUNK_SIZE = 500  # these are words
-file_country_names = "data/interim/country_names_1.txt"
