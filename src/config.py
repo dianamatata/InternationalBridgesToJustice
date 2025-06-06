5a4a9f1887f0d36c5db42c0c089aeade872b24b9
dianamatata <diana.avalos.m@gmail.com>
2025-06-06 11:48:21 +0200

    added files from other_legal_docs with metadata

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import os
 class Paths:
 
     # embeddings, collection, chromadb
@@ -11,7 +12,6 @@ class Paths:
     PATH_JSONL_FILE_DEFENSEWIKI_CHUNKS = "data/processed/defensewiki.ibj.org/chunks_1.jsonl"
     PATH_JSONL_FILE_DEFENSEWIKI = "data/interim/defensewiki_all.jsonl"
 
-
     # ConstitutionProject
     START_URL_CONSTITUTIONS = "https://www.constituteproject.org/constitutions?lang=en&status=in_force&status=is_draft"
     BASE_URL_CONSTITUTIONS = "https://www.constituteproject.org"
@@ -19,6 +19,13 @@ class Paths:
     PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES = "data/processed/constituteproject.org"
     PATH_JSONL_FILE_WITH_CONSTITUTIONS_ALL_COUNTRIES = f"{PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES}/constituteproject.jsonl"
     PATH_JSONL_FILE_CONSTITUTION_CHUNKS = "data/processed/constituteproject.org/constitution_chunks.jsonl"
+
+    # Legal documents
+    PATH_FOLDER_LEGAL_FILES_PDF = "data/raw/legal_countries_docs_pdfs"
+    PATH_FOLDER_LEGAL_FILES_JSONL = "data/processed/legal_countries_docs"
+    PATH_JSONL_FILE_WITH_LEGAL_FILES = "data/processed/legal_countries_docs/other_legal_docs.jsonl"
+    PATH_JSONL_FILE_LEGAL_OTHERS = "data/processed/legal_countries_docs/other_legal_docs_chunks.jsonl"
+
 
     # Keypoints completeness
     PATH_MD_FILE_COMPLETENESS_KEYPOINTS = "data/raw/IBJ_docs/Completeness_checklist.md"
@@ -42,3 +49,79 @@ class Paths:
 
 # Others
 MAX_CHUNK_SIZE = 500  # these are words
+
+
+legal_files_info_list = [
+    {
+        "filename": "Indian_penal_code.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Indian_penal_code.pdf"),
+        "country": "India",
+        "language": "en",
+        "legal_type": "penal_code",
+        "publication_date": "1860-10-06",
+        "year": "1860",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+    {
+        "filename": "Burundi_Penal-Code-Revised_2009_FRENCH-1.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi_Penal-Code-Revised_2009_FRENCH-1.pdf"),
+        "country": "Burundi",
+        "language": "fr",
+        "legal_type": "penal_code",
+        "publication_date": "2009-05-22",
+        "year": "2009",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+    {
+        "filename": "Accord_Arusha_Burundi.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Accord_Arusha_Burundi.pdf"),
+        "country": "Burundi",
+        "language": "fr",
+        "legal_type": "peace_agreement",
+        "publication_date": "2000-08-28",
+        "year": "2000",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+    {
+        "filename": "Burundi-Code-2018-procedure-penale.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi-Code-2018-procedure-penale.pdf"),
+        "country": "Burundi",
+        "language": "fr",
+        "legal_type": "penal_procedure",
+        "publication_date": "2018-05-11",
+        "year": "2018",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+    {
+        "filename": "Burundi_Charte-de-l-Unite-nationale-1991.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi_Charte-de-l-Unite-nationale-1991.pdf"),
+        "country": "Burundi",
+        "language": "fr",
+        "legal_type": "",
+        "publication_date": "2006-12-31",
+        "year": "2006",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+    {
+        "filename": "China_penal_code.pdf",
+        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "China_penal_code1.pdf"),
+        "country": "China",
+        "language": "en",
+        "legal_type": "",
+        "publication_date": "1997-03-14",
+        "year": "1997",
+        "legal_relevance": "national",
+        "website": "",
+        "legal_status": ""
+    },
+]
