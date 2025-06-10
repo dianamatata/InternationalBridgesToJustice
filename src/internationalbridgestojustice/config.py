6d8f45ef1ed5ef128552c5d1ef569c1944f33815
dianamatata <diana.avalos.m@gmail.com>
2025-06-10 11:04:09 +0200

    reformat ruff

--- src/internationalbridgestojustice/config.py
+++ src/internationalbridgestojustice/config.py
@@ -1,6 +1,7 @@
 import os
-class Paths:
 
+
+class Paths:
     # embeddings, collection, chromadb
     PATH_CHROMADB = "data/chroma_db"
     COLLECTION_NAME = "legal_collection"
@@ -9,7 +10,9 @@ class Paths:
     # DefenseWiki
     BASE_URL_DEFENSE_WIKI = "https://defensewiki.ibj.org"
     PATH_FOLDER_DEFENSE_WIKI = "data/processed/defensewiki.ibj.org"
-    PATH_JSONL_FILE_DEFENSEWIKI_CHUNKS = "data/processed/defensewiki.ibj.org/chunks_1.jsonl"
+    PATH_JSONL_FILE_DEFENSEWIKI_CHUNKS = (
+        "data/processed/defensewiki.ibj.org/chunks_1.jsonl"
+    )
     PATH_JSONL_FILE_DEFENSEWIKI = "data/interim/defensewiki_all.jsonl"
 
     # ConstitutionProject
@@ -17,32 +20,48 @@ class Paths:
     BASE_URL_CONSTITUTIONS = "https://www.constituteproject.org"
     PATH_FOLDER_CONSTITUTIONS_WITH_MD_FILES = "data/raw/constituteproject.org"
     PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES = "data/processed/constituteproject.org"
-    PATH_JSONL_FILE_WITH_CONSTITUTIONS_ALL_COUNTRIES = f"{PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES}/constituteproject.jsonl"
-    PATH_JSONL_FILE_CONSTITUTION_CHUNKS = "data/processed/constituteproject.org/constitution_chunks.jsonl"
+    PATH_JSONL_FILE_WITH_CONSTITUTIONS_ALL_COUNTRIES = (
+        f"{PATH_FOLDER_CONSTITUTIONS_WITH_JSONL_FILES}/constituteproject.jsonl"
+    )
+    PATH_JSONL_FILE_CONSTITUTION_CHUNKS = (
+        "data/processed/constituteproject.org/constitution_chunks.jsonl"
+    )
 
     # Legal documents
     PATH_FOLDER_LEGAL_FILES_PDF = "data/raw/legal_countries_docs_pdfs"
     PATH_FOLDER_LEGAL_FILES_JSONL = "data/processed/legal_countries_docs"
-    PATH_JSONL_FILE_WITH_LEGAL_FILES = "data/processed/legal_countries_docs/other_legal_docs.jsonl"
-    PATH_JSONL_FILE_LEGAL_OTHERS = "data/processed/legal_countries_docs/other_legal_docs_chunks.jsonl"
+    PATH_JSONL_FILE_WITH_LEGAL_FILES = (
+        "data/processed/legal_countries_docs/other_legal_docs.jsonl"
+    )
+    PATH_JSONL_FILE_LEGAL_OTHERS = (
+        "data/processed/legal_countries_docs/other_legal_docs_chunks.jsonl"
+    )
 
+    # Translated chunks
+    PATH_TRANSLATED_CHUNKS = "data/translated_chunks/Burundi_chunks_in_english.jsonl"
 
     # Keypoints completeness
     PATH_MD_FILE_COMPLETENESS_KEYPOINTS = "data/raw/IBJ_docs/Completeness_checklist.md"
     PATH_JSON_FILE_COMPLETENESS_KEYPOINTS = "data/completeness/keypoints.json"
-    PATH_JSON_FILE_DESCRIPTIVE_COMPLETENESS_KEYPOINTS = "data/completeness/descriptive_keypoints.json"
+    PATH_JSON_FILE_DESCRIPTIVE_COMPLETENESS_KEYPOINTS = (
+        "data/completeness/descriptive_keypoints.json"
+    )
     PATH_FOLDER_COMPLETENESS = "data/completeness"
 
     # Claim extraction
     PATH_FOLDER_CLAIM_EXTRACTION = "data/extracted_claims"
-    PATH_JSONL_FILE_EXTRACTED_CLAIMS = f"{PATH_FOLDER_CLAIM_EXTRACTION}/claims_chunks_1.jsonl"
+    PATH_JSONL_FILE_EXTRACTED_CLAIMS = (
+        f"{PATH_FOLDER_CLAIM_EXTRACTION}/claims_chunks_1.jsonl"
+    )
 
     # Prompts
     PATH_PROMPTS = "data/prompts"
     PATH_FILE_PROMPT_COMPLETENESS = f"{PATH_PROMPTS}/prompt_completeness.md"
     PATH_FILE_PROMPT_CLAIM_EXTRACTION = f"{PATH_PROMPTS}/prompt_claim_extraction.md"
     PATH_FILE_PROMPT_CLAIM_VERIFICATION = f"{PATH_PROMPTS}/prompt_claim_verification.md"
-    PATH_FILE_SYSTEM_PROMPT_COMPLETENESS = f"{PATH_PROMPTS}/system_prompt_completeness.md"
+    PATH_FILE_SYSTEM_PROMPT_COMPLETENESS = (
+        f"{PATH_PROMPTS}/system_prompt_completeness.md"
+    )
 
     FILE_COUNTRY_NAMES = "data/interim/country_names_1.txt"
 
@@ -54,7 +73,9 @@ MAX_CHUNK_SIZE = 500  # these are words
 legal_files_info_list = [
     {
         "filename": "Indian_penal_code.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Indian_penal_code.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Indian_penal_code.pdf"
+        ),
         "country": "India",
         "language": "en",
         "legal_type": "penal_code",
@@ -62,11 +83,14 @@ legal_files_info_list = [
         "year": "1860",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
     {
         "filename": "Burundi_Penal-Code-Revised_2009_FRENCH-1.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi_Penal-Code-Revised_2009_FRENCH-1.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF,
+            "Burundi_Penal-Code-Revised_2009_FRENCH-1.pdf",
+        ),
         "country": "Burundi",
         "language": "fr",
         "legal_type": "penal_code",
@@ -74,11 +98,13 @@ legal_files_info_list = [
         "year": "2009",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
     {
         "filename": "Accord_Arusha_Burundi.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Accord_Arusha_Burundi.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Accord_Arusha_Burundi.pdf"
+        ),
         "country": "Burundi",
         "language": "fr",
         "legal_type": "peace_agreement",
@@ -86,11 +112,13 @@ legal_files_info_list = [
         "year": "2000",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
     {
         "filename": "Burundi-Code-2018-procedure-penale.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi-Code-2018-procedure-penale.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi-Code-2018-procedure-penale.pdf"
+        ),
         "country": "Burundi",
         "language": "fr",
         "legal_type": "penal_procedure",
@@ -98,11 +126,14 @@ legal_files_info_list = [
         "year": "2018",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
     {
         "filename": "Burundi_Charte-de-l-Unite-nationale-1991.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "Burundi_Charte-de-l-Unite-nationale-1991.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF,
+            "Burundi_Charte-de-l-Unite-nationale-1991.pdf",
+        ),
         "country": "Burundi",
         "language": "fr",
         "legal_type": "",
@@ -110,11 +141,13 @@ legal_files_info_list = [
         "year": "2006",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
     {
         "filename": "China_penal_code.pdf",
-        "path": os.path.join(Paths.PATH_FOLDER_LEGAL_FILES_PDF, "China_penal_code1.pdf"),
+        "path": os.path.join(
+            Paths.PATH_FOLDER_LEGAL_FILES_PDF, "China_penal_code1.pdf"
+        ),
         "country": "China",
         "language": "en",
         "legal_type": "",
@@ -122,6 +155,6 @@ legal_files_info_list = [
         "year": "1997",
         "legal_relevance": "national",
         "website": "",
-        "legal_status": ""
+        "legal_status": "",
     },
 ]
