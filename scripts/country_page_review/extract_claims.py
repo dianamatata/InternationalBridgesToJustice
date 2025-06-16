85938c4115b9369e527ce1da5d5768fa68ffcbc8
dianamatata <diana.avalos.m@gmail.com>
2025-06-16 14:27:35 +0200

    clean to pipe from completeness

--- scripts/country_page_review/extract_claims.py
+++ scripts/country_page_review/extract_claims.py
@@ -1,103 +1,104 @@
-import os
 import json
-import numpy as np
-from tqdm import tqdm
 from src.internationalbridgestojustice.get_claims import ClaimExtractor
 from src.internationalbridgestojustice.config import Paths
-from src.internationalbridgestojustice.file_manager import (
-    load_jsonl_and_convert_to_list_of_dict,
+from src.internationalbridgestojustice.file_manager import save_file
+from src.internationalbridgestojustice.openai_utils import (
+    openai_client,
+    upload_batch_file_to_openAI,
+    submit_batch_job,
+    check_progress_batch_id,
+    save_batch_id,
+    retrieve_and_save_batch_results,
+    retrieve_tool_calls,
 )
 
-model_name = "gpt-4o-mini"
-cache_dir = "./data/cache"
+# extract from output of completeness script
+claim_extractor = ClaimExtractor()
+country = "Burundi"
+input_file_path_jsonl = (
+    f"{Paths.PATH_FOLDER_COMPLETENESS}/batch_output_completeness_Burundi_all.jsonl"
+)
+output_file_path_jsonl = f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/claims_Burundi.jsonl"
 
-data = load_jsonl_and_convert_to_list_of_dict(Paths.PATH_JSONL_FILE_DEFENSEWIKI_CHUNKS)
-country_list = ["Burundi"]
-data_country = [d for d in data if "Burundi" == d["metadata"]["country"]]
-
-links_list_to_extract = []
-for item in data:
-    title = item["metadata"]["country"]
-    if any(country in title for country in country_list):
-        links_list_to_extract.append(item["metadata"]["link"])
-
-links_list_to_extract = np.unique(links_list_to_extract)
-print(links_list_to_extract)
-
-claim_extractor = ClaimExtractor(
-    model_name=model_name,
-    prompt_file=Paths.PATH_FILE_PROMPT_CLAIM_EXTRACTION,
-    cache_dir=cache_dir,
+jsonl_file_claim_extraction_batch = (
+    f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_input_extraction_Burundi_all.jsonl"
+)
+jsonl_file_claim_extraction_batch_output = (
+    f"{Paths.PATH_FOLDER_COMPLETENESS}/batch_output_extraction_Burundi_all.jsonl"
 )
 
-# extract a page -------------------------------------
-page = links_list_to_extract[0]
-chunks_to_extract = []
-for dict_item in tqdm(data):
-    if dict_item["metadata"]["link"] == page:
-        chunks_to_extract.append(dict_item)
-        for chunk in chunks_to_extract:
-            response = chunk["content"]
-            prompt_source = chunk["title"]
-            print(prompt_source)
-            title = chunk["metadata"]["title"]
-            snippet_lst, claim_list, all_claims, prompt_tok_cnt, response_tok_cnt = (
-                claim_extractor.scan_text_for_claims(response)
-            )
-            output_dict = {
-                "prompt_source": prompt_source,
-                "response": response.strip(),
-                "prompt_tok_cnt": prompt_tok_cnt,
-                "response_tok_cnt": response_tok_cnt,
-                "model": model_name,
-                "abstained": False,  # "abstained": False, "abstained": True
-                "claim_list": claim_list,
-                "all_claims": all_claims,
-            }
-            filename = (
-                f"{title.replace(' ', '_').replace('/', '_').replace(':', '_')}.jsonl"
-            )
-            jsonl_file_extracted_claims = os.path.join(
-                Paths.PATH_FOLDER_CLAIM_EXTRACTION, filename
-            )  # Specify your output directory
-            with open(
-                jsonl_file_extracted_claims, "a"
-            ) as jsonl_file:  # a appends, w overwrites
-                jsonl_file.write(json.dumps(output_dict) + "\n")
-        print(f"extracted claims are saved at {Paths.PATH_JSONL_FILE_EXTRACTED_CLAIMS}")
+completeness_keypoints = []
+with open(input_file_path_jsonl, "r", encoding="utf-8") as file:
+    for line in file:
+        completeness_keypoints.append(json.loads(line))
 
+batch_submission = False
 
-# extract a country ----------------------------------
-country_list = ["India"]
-for dict_item in tqdm(data):
-    title = dict_item["metadata"]["title"]
-    if any(country in title for country in country_list):
-        print(title)
-        response = dict_item["content"]
-        prompt_source = dict_item["title"]
-        print(prompt_source)
-        snippet_lst, claim_list, all_claims, prompt_tok_cnt, response_tok_cnt = (
+if batch_submission == False:
+    for keypoint in completeness_keypoints:
+        keypoint = completeness_keypoints[1]
+        print(keypoint["Rewritten_Wiki_Chapter"])
+        response = keypoint["Rewritten_Wiki_Chapter"]
+
+        snippet_lst, claim_list, all_claims, total_cost = (
             claim_extractor.scan_text_for_claims(response)
         )
         output_dict = {
-            "prompt_source": prompt_source,
+            "country": keypoint["Country"],
+            "keypoint": keypoint["Keypoint"],
             "response": response.strip(),
-            "prompt_tok_cnt": prompt_tok_cnt,
-            "response_tok_cnt": response_tok_cnt,
-            "model": model_name,
-            "abstained": False,  # "abstained": False, "abstained": True
+            "total_cost": total_cost,
             "claim_list": claim_list,
             "all_claims": all_claims,
         }
 
-        filename = (
-            f"{title.replace(' ', '_').replace('/', '_').replace(':', '_')}.jsonl"
-        )
-        jsonl_file_extracted_claims = os.path.join(
-            Paths.PATH_FOLDER_CLAIM_EXTRACTION, filename
-        )  # Specify your output directory
-        with open(
-            jsonl_file_extracted_claims, "a"
-        ) as jsonl_file:  # a appends, w overwrites
+        with open(output_file_path_jsonl, "a") as jsonl_file:  # a appends, w overwrites
             jsonl_file.write(json.dumps(output_dict) + "\n")
-print(f"extracted claims are saved at {Paths.PATH_JSONL_FILE_EXTRACTED_CLAIMS}")
+
+    print(f"extracted claims are saved at {Paths.PATH_JSONL_FILE_EXTRACTED_CLAIMS}")
+
+elif batch_submission == True:
+    for keypoint in completeness_keypoints:
+        print(keypoint["Rewritten_Wiki_Chapter"])
+        response = keypoint["Rewritten_Wiki_Chapter"]
+
+        claim_extractor.create_batch_file_for_extraction(
+            response=response,
+            country=country,
+            keypoint=keypoint["Keypoint"],
+            jsonl_output_file_path=jsonl_file_claim_extraction_batch,
+        )
+
+        file = upload_batch_file_to_openAI(
+            client=openai_client,
+            batch_file_name=jsonl_file_claim_extraction_batch,
+        )
+        batch = submit_batch_job(client=openai_client, file_id=file.id)
+        save_batch_id(
+            batch=batch,
+            country=country,
+            path_file=f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_id_extraction.txt",
+        )
+
+        # RETRIEVE RESULTS ---------------------------------------------------
+
+        # TODO retrieve results in other script
+        batch_id = "batch_685009985fa48190961415687c207498"  # test burundi
+        print(f"Batch job submitted: {batch_id}")
+
+        check_progress_batch_id(batch_id=batch_id)
+        # if failed:
+        # result = openai_client.batches.retrieve(batch_id=batch_id)
+
+        parsed_results = retrieve_and_save_batch_results(
+            batch_id=batch_id,
+            output_file_path_jsonl="data/interim/claimExtraction_Burundi_results.jsonl",
+            return_parsed_results=True,
+        )
+        results_list = retrieve_tool_calls(parsed_results)
+
+        save_file(
+            filename=jsonl_file_claim_extraction_batch_output,
+            content=results_list,
+            file_type="jsonl1",
+        )
