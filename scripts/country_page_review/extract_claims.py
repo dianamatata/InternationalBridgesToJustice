dd7fc89d8b55f3c1bbd657775fab8823c7ed11b8
dianamatata <diana.avalos.m@gmail.com>
2025-06-17 12:01:25 +0200

    filenames edit

--- scripts/country_page_review/extract_claims.py
+++ scripts/country_page_review/extract_claims.py
@@ -13,28 +13,30 @@ from src.internationalbridgestojustice.openai_utils import (
 )
 
 # extract from output of completeness script
-claim_extractor = ClaimExtractor()
 country = "Burundi"
-input_file_path_jsonl = (
-    f"{Paths.PATH_FOLDER_COMPLETENESS}/batch_output_completeness_Burundi_all.jsonl"
+input_file_from_completeness_jsonl = (
+    "data/completeness/batch_output_completeness_Burundi_all.jsonl"
 )
-output_file_path_jsonl = f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/claims_Burundi.jsonl"
+output_file_path_jsonl = (
+    f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/claims_extraction_Burundi_test.jsonl"
+)
 
 jsonl_file_claim_extraction_batch = (
     f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_input_extraction_Burundi_all.jsonl"
 )
 jsonl_file_claim_extraction_batch_output = (
-    f"{Paths.PATH_FOLDER_COMPLETENESS}/batch_output_extraction_Burundi_all.jsonl"
+    f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_output_extraction_Burundi_all.jsonl"
 )
 
 completeness_keypoints = []
-with open(input_file_path_jsonl, "r", encoding="utf-8") as file:
+with open(input_file_from_completeness_jsonl, "r", encoding="utf-8") as file:
     for line in file:
         completeness_keypoints.append(json.loads(line))
 
 batch_submission = False
 
 if batch_submission == False:
+    claim_extractor = ClaimExtractor()
     for keypoint in completeness_keypoints:
         keypoint = completeness_keypoints[1]
         print(keypoint["Rewritten_Wiki_Chapter"])
@@ -58,47 +60,50 @@ if batch_submission == False:
     print(f"extracted claims are saved at {Paths.PATH_JSONL_FILE_EXTRACTED_CLAIMS}")
 
 elif batch_submission == True:
+    claim_extractor = ClaimExtractor()
     for keypoint in completeness_keypoints:
-        print(keypoint["Rewritten_Wiki_Chapter"])
-        response = keypoint["Rewritten_Wiki_Chapter"]
-
         claim_extractor.create_batch_file_for_extraction(
-            response=response,
+            custom_id=keypoint["custom_id"].replace(
+                "completeness", "claimextraction", 1
+            ),
+            response=keypoint["Rewritten_Wiki_Chapter"],
             country=country,
             keypoint=keypoint["Keypoint"],
             jsonl_output_file_path=jsonl_file_claim_extraction_batch,
         )
 
-        file = upload_batch_file_to_openAI(
-            client=openai_client,
-            batch_file_name=jsonl_file_claim_extraction_batch,
-        )
-        batch = submit_batch_job(client=openai_client, file_id=file.id)
-        save_batch_id(
-            batch=batch,
-            country=country,
-            path_file=f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_id_extraction.txt",
-        )
+    file = upload_batch_file_to_openAI(
+        client=openai_client,
+        batch_file_name=jsonl_file_claim_extraction_batch,
+    )
+    batch = submit_batch_job(client=openai_client, file_id=file.id)
+    save_batch_id(
+        batch=batch,
+        country=country,
+        path_file=f"{Paths.PATH_FOLDER_CLAIM_EXTRACTION}/batch_id_extraction.txt",
+    )
 
-        # RETRIEVE RESULTS ---------------------------------------------------
+    # RETRIEVE RESULTS ---------------------------------------------------
 
-        # TODO retrieve results in other script
-        batch_id = "batch_685009985fa48190961415687c207498"  # test burundi
-        print(f"Batch job submitted: {batch_id}")
+    # TODO retrieve results in other script
+    # TODO do we really need to run openAI per sentence. can we just write a prompt that also does the sentence separation?
+    batch_id = "batch_685009985fa48190961415687c207498"  # test burundi
+    batch_id = "batch_6851200fb0248190b5e8917a5e8a22c7"  # all burundi new
 
-        check_progress_batch_id(batch_id=batch_id)
-        # if failed:
-        # result = openai_client.batches.retrieve(batch_id=batch_id)
+    print(f"Batch job submitted: {batch_id}")
 
-        parsed_results = retrieve_and_save_batch_results(
-            batch_id=batch_id,
-            output_file_path_jsonl="data/interim/claimExtraction_Burundi_results.jsonl",
-            return_parsed_results=True,
-        )
-        results_list = retrieve_tool_calls(parsed_results)
+    check_progress_batch_id(batch_id=batch_id)
+    # result = openai_client.batches.retrieve(batch_id=batch_id) # if failed:
 
-        save_file(
-            filename=jsonl_file_claim_extraction_batch_output,
-            content=results_list,
-            file_type="jsonl1",
-        )
+    parsed_results = retrieve_and_save_batch_results(
+        batch_id=batch_id,
+        output_file_path_jsonl="data/extracted_claims/claimExtraction_Burundi_results_all.jsonl",
+        return_parsed_results=True,
+    )
+    results_list = retrieve_tool_calls(parsed_results)
+
+    save_file(
+        filename=jsonl_file_claim_extraction_batch_output,
+        content=results_list,
+        file_type="jsonl1",
+    )
